1856d5dafba281b5d183f9a496071cea75e5ce3f
armstrhb <henry.b.armstrong@gmail.com>
2015-08-04 22:43:30 -0400

    experience bank interface stub

--- src/org/laera/experience/SimilarityEngine.java
+++ src/org/laera/experience/SimilarityEngine.java
@@ -1,5 +1,9 @@
 package org.laera.experience;
 
 public class SimilarityEngine {
-
+	ExperienceBank experienceBank;
+	
+	public void setExperienceBank(ExperienceBank bank) {
+		experienceBank = bank;
+	}
 }
